87487dd08deaca1ebad9bb2370bb323211c79c02
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2020-10-09 12:43:46 +0300

    Fix CreateDocument()

--- framework/src/main/kotlin/info/anodsplace/framework/content/ActivityResult.kt
+++ framework/src/main/kotlin/info/anodsplace/framework/content/ActivityResult.kt
@@ -6,23 +6,30 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.provider.DocumentsContract
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import info.anodsplace.framework.AppLog
 
-fun CreateDocument(initialUri: Uri, dataType: String) = object: ActivityResultContracts.CreateDocument() {
-
+class CreateDocument : ActivityResultContract<CreateDocument.Args, Uri>() {
+    private val inner = ActivityResultContracts.CreateDocument()
+    class Args(val initialUri: Uri, val dataType: String, val title: String)
+    
     @SuppressLint("MissingSuperCall")
-    override fun createIntent(context: Context, input: String): Intent {
+    override fun createIntent(context: Context, input: Args): Intent {
         return Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
-            putExtra(Intent.EXTRA_TITLE, input)
+            putExtra(Intent.EXTRA_TITLE, input.title)
             try {
-                setDataAndType(initialUri, dataType)
+                setDataAndType(input.initialUri, input.dataType)
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    putExtra(DocumentsContract.EXTRA_INITIAL_URI, initialUri)
+                    putExtra(DocumentsContract.EXTRA_INITIAL_URI, input.initialUri)
                 }
             } catch (e: Exception) {
                 AppLog.e(e)
             }
         }
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+        return inner.parseResult(resultCode, intent)
     }
 }
