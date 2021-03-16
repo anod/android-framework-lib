1bc311417b1c510cbd975b1bdbe3adbfa16633b1
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2021-03-16 22:09:15 +0200

    remove app compat

--- framework/build.gradle.kts
+++ framework/build.gradle.kts
@@ -34,7 +34,6 @@ android {
 
 dependencies {
     implementation("androidx.palette:palette-ktx:1.0.0")
-    implementation("androidx.appcompat:appcompat:1.3.0-beta01")
     implementation("androidx.activity:activity-ktx:1.2.1")
     implementation("androidx.fragment:fragment-ktx:1.3.1")
     implementation("com.google.android.material:material:1.3.0")
