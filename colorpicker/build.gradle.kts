82cc32cd7937ac181429be0408cadc610bcbbe93
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2021-05-01 12:18:11 +0300

    upate dependnecies

--- colorpicker/build.gradle.kts
+++ colorpicker/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 }
 
 repositories {
+    jcenter()
     google()
 }
 
@@ -22,5 +23,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.3.0-rc01")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.fragment:fragment:1.3.3")
 }
