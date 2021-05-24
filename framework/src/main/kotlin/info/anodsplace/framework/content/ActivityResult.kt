341342f02d4ec356b843d9edddf68ba2533ead8b
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2021-05-24 11:04:37 +0300

    make input.initialUri optiuonal

--- framework/src/main/kotlin/info/anodsplace/framework/content/ActivityResult.kt
+++ framework/src/main/kotlin/info/anodsplace/framework/content/ActivityResult.kt
@@ -10,18 +10,20 @@ import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import info.anodsplace.applog.AppLog
 
-class CreateDocument : ActivityResultContract<CreateDocument.Args, Uri>() {
+class CreateDocument : ActivityResultContract<CreateDocument.Args, Uri?>() {
     private val inner = ActivityResultContracts.CreateDocument()
-    class Args(val initialUri: Uri, val dataType: String, val title: String)
+    class Args(val dataType: String, val title: String?, val initialUri: Uri? = null)
     
     @SuppressLint("MissingSuperCall")
     override fun createIntent(context: Context, input: Args): Intent {
         return Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
             putExtra(Intent.EXTRA_TITLE, input.title)
             try {
-                setDataAndType(input.initialUri, input.dataType)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    putExtra(DocumentsContract.EXTRA_INITIAL_URI, input.initialUri)
+                if (input.initialUri != null) {
+                    setDataAndType(input.initialUri, input.dataType)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        putExtra(DocumentsContract.EXTRA_INITIAL_URI, input.initialUri)
+                    }
                 }
             } catch (e: Exception) {
                 AppLog.e(e)
