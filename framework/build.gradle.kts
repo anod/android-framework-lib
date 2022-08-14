508018eaf2433022188f02b7ac2edf6fc2d062fb
Alexander Gavrishev <algavris@microsoft.com>
2022-08-14 21:37:45 +0300

    Upgrade lib

--- framework/build.gradle.kts
+++ framework/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 dependencies {
     implementation(project(":lib:applog"))
     implementation("androidx.activity:activity-ktx:1.5.1")
-    implementation("androidx.fragment:fragment-ktx:1.5.1")
+    implementation("androidx.fragment:fragment-ktx:1.5.2")
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.window:window:1.1.0-alpha03")
     implementation("androidx.core:core-ktx:1.8.0")
