91642abea5b697feb886ddb46d7accd3eeb64266
Alexander Gavrishev <algavris@microsoft.com>
2022-10-07 12:36:12 +0300

    Add ByteArray?.toBitmap

--- graphics/src/main/kotlin/info/anodsplace/graphics/BitmapByteArray.kt
+++ graphics/src/main/kotlin/info/anodsplace/graphics/BitmapByteArray.kt
@@ -2,34 +2,11 @@ package info.anodsplace.graphics
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.Log
 
-import java.io.ByteArrayOutputStream
-import java.io.IOException
-
-object BitmapByteArray {
-    fun flatten(bitmap: Bitmap): ByteArray? {
-        // Try go guesstimate how much space the icon will take when serialized
-        // to avoid unnecessary allocations/copies during the write.
-        val size = bitmap.width * bitmap.height * 4
-        val out = ByteArrayOutputStream(size)
-        return try {
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
-            out.flush()
-            out.close()
-            out.toByteArray()
-        } catch (e: IOException) {
-            Log.w("Favorite", "Could not write icon")
-            null
-        }
+fun ByteArray?.toBitmap(opts: BitmapFactory.Options): Bitmap? {
+    val data = this ?: return null
+    if (data.isEmpty()) {
+        return null
     }
-
-    fun unflatten(bitmapData: ByteArray?): Bitmap? {
-        var bitmap: Bitmap? = null
-        if (bitmapData != null && bitmapData.isNotEmpty()) {
-            bitmap = BitmapFactory.decodeByteArray(bitmapData, 0, bitmapData.size)
-        }
-        return bitmap
-    }
-
+    return BitmapFactory.decodeByteArray(data, 0, data.size, opts)
 }
