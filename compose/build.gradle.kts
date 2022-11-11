70f3a9869923501ef6b33bb14ae7cacdebfdc449
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2022-11-11 17:42:18 +0200

    Upgrade libs

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -46,16 +46,16 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation("com.google.accompanist:accompanist-flowlayout:0.27.0")
-    api("androidx.compose.ui:ui:1.3.0")
-    api("androidx.compose.material3:material3:1.0.0")
-    api("androidx.compose.foundation:foundation:1.3.0")
-    api("androidx.compose.material:material-icons-core:1.3.0")
-    api("androidx.compose.material:material-icons-extended:1.3.0")
+    api("androidx.compose.ui:ui:1.3.1")
+    api("androidx.compose.material3:material3:1.0.1")
+    api("androidx.compose.foundation:foundation:1.3.1")
+    api("androidx.compose.material:material-icons-core:1.3.1")
+    api("androidx.compose.material:material-icons-extended:1.3.1")
 
-    debugApi("androidx.compose.ui:ui-tooling:1.3.0")
+    debugApi("androidx.compose.ui:ui-tooling:1.3.1")
     debugApi("androidx.customview:customview:1.2.0-alpha02")
     debugApi("androidx.customview:customview-poolingcontainer:1.0.0")
-    api("androidx.compose.ui:ui-tooling-preview:1.3.0")
+    api("androidx.compose.ui:ui-tooling-preview:1.3.1")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.20")
 }
