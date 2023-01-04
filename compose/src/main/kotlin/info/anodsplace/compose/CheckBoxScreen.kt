5b49b037c8b17649f3ab46a6082ef076b0d4cb33
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2023-01-04 11:13:55 +0200

    Update CheckBoxScreen

--- compose/src/main/kotlin/info/anodsplace/compose/CheckBoxScreen.kt
+++ compose/src/main/kotlin/info/anodsplace/compose/CheckBoxScreen.kt
@@ -1,53 +1,43 @@
 package info.anodsplace.compose
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.toggleable
-import androidx.compose.material3.*
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-class CheckBoxScreenState(
-        val title: String,
-        val items: Map<String, Any?>,
-        val checked: SnapshotStateList<String>
-)
-
 @Composable
-fun CheckBoxList(items: Map<String, Any?>, checked: SnapshotStateList<String>, modifier: Modifier = Modifier, onCheckedChange: (key: String, value: Any?, checked: Boolean) -> Unit) {
-    val checkedMap = checked.associateWith { true }
+fun CheckBoxList(items: Map<String, Boolean>, modifier: Modifier = Modifier, onCheckedChange: (key: String, checked: Boolean) -> Unit) {
     rememberScrollState(0)
     LazyColumn(modifier = modifier) {
         items(items.entries.toList()) { item ->
-            val isItemChecked = checkedMap.containsKey(item.key)
-            val (itemChecked, onItemChecked) = remember { mutableStateOf(isItemChecked) }
+            val isItemChecked = item.value
             Row(modifier = Modifier
-                    .toggleable(value = itemChecked, onValueChange = onItemChecked)
+                    .toggleable(value = isItemChecked, onValueChange = { newState ->
+                        onCheckedChange(item.key, newState)
+                    })
             ) {
                 Checkbox(
                         modifier = Modifier.align(Alignment.CenterVertically),
-                        checked = itemChecked,
+                        checked = isItemChecked,
                         onCheckedChange = { newState ->
-                            if (checkedMap.containsKey(item.key)) {
-                                if (!newState) {
-                                    checked.remove(item.key)
-                                }
-                            } else {
-                                if (newState) {
-                                    checked.add(item.key)
-                                }
-                            }
-                            onItemChecked(newState)
+                            onCheckedChange(item.key, newState)
                         },
                 )
                 Text(
@@ -68,50 +58,42 @@ fun CheckBoxList(items: Map<String, Any?>, checked: SnapshotStateList<String>, m
 }
 
 @Composable
-fun CheckBoxScreen(saveText: String, state: CheckBoxScreenState, onDismissRequest: () -> Unit) {
+fun CheckBoxScreen(
+    titleText: String,
+    saveText: String,
+    items: Map<String, Boolean>,
+    onCheckedChange: (key: String, checked: Boolean) -> Unit,
+    onDismissRequest: () -> Unit
+) {
     Surface(color = MaterialTheme.colorScheme.background) {
         Column(modifier = Modifier.fillMaxHeight()) {
             Box(modifier = Modifier
                     .padding(16.dp)
                     .align(Alignment.Start)) {
-                // CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
-                Text(state.title, style = MaterialTheme.typography.titleMedium)
-                // }
+                Text(titleText, style = MaterialTheme.typography.titleMedium)
             }
             CheckBoxList(
-                    items = state.items,
-                    checked = state.checked,
-                    modifier = Modifier
-                            .padding(horizontal = 16.dp)
-                            .weight(0.1f, fill = true),
-            ) { _: String, _: Any?, _: Boolean -> }
+                items = items,
+                modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .weight(0.1f, fill = true),
+                onCheckedChange = onCheckedChange
+            )
             ButtonsPanel(actionText = saveText, onDismissRequest = onDismissRequest, onAction = onDismissRequest)
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnrememberedMutableState")
 @Preview
 @Composable
 fun CheckBoxScreenPreview() {
-    val state = CheckBoxScreenState(
-            "Categories", emptyMap(), mutableStateListOf("ACTIVITY_NEW_TASK", "ACTIVITY_NEW_DOCUMENT")
-    )
     MaterialTheme {
-        Scaffold(
-                topBar = {
-                    CenterAlignedTopAppBar(title = {
-                        Text(text = "Preview")
-                    })
-                },
-                containerColor = MaterialTheme.colorScheme.background,
-                content = {
-                    Box(modifier = Modifier.padding(it),
-                            contentAlignment = Alignment.Center) {
-                        CheckBoxScreen(saveText = "Save", state, onDismissRequest = {})
-                    }
-                }
+        CheckBoxScreen(
+            titleText = "Categories",
+            saveText = "Save",
+            items = mapOf("ACTIVITY_NEW_TASK" to false, "ACTIVITY_NEW_DOCUMENT" to true),
+            onDismissRequest = {},
+            onCheckedChange = { _, _ -> }
         )
     }
 }
