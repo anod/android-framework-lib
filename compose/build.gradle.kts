79071ba22674b89c634b42c76a4f8e094b78dc71
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2023-12-23 09:43:41 +0200

    Update kotlinCompilerExtensionVersion

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.6"
+        kotlinCompilerExtensionVersion = "1.5.7"
     }
     namespace = "info.anodsplace.compose"
 }
