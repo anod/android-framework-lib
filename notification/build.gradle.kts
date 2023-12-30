9e05880a8acf4128cd6316d88681b0cdb54c57a2
Alexandr Gavrishev <algavris@microsoft.com>
2023-12-30 18:03:06 +0200

    Update plugins to use versions catalog

--- notification/build.gradle.kts
+++ notification/build.gradle.kts
@@ -1,6 +1,6 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin)
 }
 
